b01ed14e12ebfa430ff144a4833b717519faca18
afluffysquirrel <afluffysquirrel@gmail.com>
2024-06-12 23:49:38 +0100

    feat: improved home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,33 @@ export default function Page() {
   return (
     <StoreLayout>
       <HeroSection />
-      <div className="flex flex-col justify-center">
-        <div className="relative top-0 py-0 sm:-top-10 sm:mx-auto sm:max-w-2xl sm:py-3">
-          <div className="absolute inset-0 z-10 hidden -rotate-6 skew-y-0 rounded-3xl bg-gradient-to-r from-teal-500 to-cyan-600 shadow-lg sm:inline"></div>
-          <div className="relative z-20 rounded-3xl bg-white p-6 sm:bg-slate-50 sm:p-8 sm:shadow-lg">
-            <h1 className="mb-2 text-2xl font-bold text-gray-800">About Us</h1>
-            <p className="text-gray-500">
-              Our business is dedicated to providing quality training gear and clothing designed specifically for
-              climbers. We aim to help climbers reach their full potential by offering products that enhance their
-              training experience and performance. Whether you&apos;re just starting out or are a seasoned climber, our
-              gear is crafted to meet your needs and help you achieve your climbing goals.
-            </p>
+
+      <div className="relative top-0 mx-auto my-0 max-w-4xl py-0 sm:max-w-2xl sm:py-3 lg:-top-10">
+        <div className="absolute inset-0 z-10 hidden -rotate-3 skew-y-0 rounded-3xl bg-gradient-to-r from-teal-500 to-cyan-600 shadow-lg md:inline md:-rotate-6"></div>
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className="m-2 flex flex-col justify-center">
+            <div className="relative z-20 rounded-3xl bg-white px-6 pt-6 md:bg-slate-100 md:p-8 md:shadow-lg">
+              <h1 className="mb-2 text-2xl font-bold text-gray-800">About us 👋</h1>
+              <p className="text-gray-500">
+                We are a UK based business who are dedicated to providing quality training gear and clothing designed
+                specifically for climbers. We aim to help climbers reach their full potential by offering products that
+                enhance their training experience and performance. Whether you&apos;re just starting out or are a
+                seasoned climber, our gear is crafted to meet your needs and help you achieve your climbing goals.
+              </p>
+            </div>
+          </div>
+          <div className="m-2 flex flex-col justify-center">
+            <div className="absolute inset-0 z-30 hidden"></div>
+            <div className="relative z-40 rounded-3xl bg-white p-6 md:bg-slate-100 md:p-8 md:shadow-lg">
+              <h1 className="mb-2 text-2xl font-bold text-gray-800">Our goal 🏋🏽‍♂️</h1>
+              <p className="text-gray-500">
+                Climbing? It&apos;s not just a sport to us. it&apos;s a whole vibe that calls for real grit and skill.
+                We&apos;re here to help you tackle those tough gravity days head-on, with all the top-notch gear and
+                advice you need to bring your A-game. Whether you&apos;re working on nailing a tricky V3 at your local
+                gym or conquering an 8a in Spain, we&apos;ve got your back with everything from finger training tips to
+                stretching guides and beyond!
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -5,7 +5,7 @@ export function HeroSection() {
     <>
       <div className="bg-container bg-[url('/images/bg.jpeg')] bg-cover bg-no-repeat text-white">
         <div className="bg-black/50 shadow-[inset_0_0px_24px_rgba(0,0,0,0.2)]">
-          <section className="mx-auto max-w-2xl py-12 md:py-24">
+          <section className="mx-auto max-w-2xl py-10 md:py-24">
             <div className="text-center">
               <h1 className="z-10 px-1 text-4xl tracking-tight md:text-5xl">Climbing, Training, Gear & Apparel</h1>
               <p className="z-[999] mt-6 hidden px-2 text-sm leading-8 sm:text-lg md:block">
@@ -15,7 +15,7 @@ export function HeroSection() {
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <NextLink
                 href="/products"
-                className="z-50 rounded-md bg-[#0080fe] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#1466b7] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="z-10 rounded-md bg-[#0080fe] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#1466b7] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Browse Products
               </NextLink>
